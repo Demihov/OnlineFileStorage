6b358ba49c5e608fdc21a421dd54baf9131b9c32
Ihor Tarasov <53966724+ihor-tarasov@users.noreply.github.com>
2023-02-11 19:26:22 +0200

    Translation stuff

--- FileStorage/BLL/AuthOptions.cs
+++ FileStorage/BLL/AuthOptions.cs
@@ -7,10 +7,10 @@ namespace BLL
 {
     public class AuthOptions
     {
-        public const string ISSUER = "MyAuthServer"; // издатель токена
-        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
-        const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 5; // время жизни токена - 5 минут
+        public const string ISSUER = "MyAuthServer"; // Token issuer
+        public const string AUDIENCE = "MyAuthClient"; // Token audience
+        const string KEY = "mysupersecret_secretkey!123";   // Encryption key
+        public const int LIFETIME = 5; // Token lifetime in minutes 
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
